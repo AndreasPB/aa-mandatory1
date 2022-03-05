712f040679147b3be7500d3d14b0ab0c16c441a7
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-03-05 14:05:20 +0100

    Clean-up haircut dataset

---
+++ src/haircut_cleanup.py
@@ -0,0 +1,38 @@
+# %%
+# import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# %%
+df = pd.read_excel("../haircut.xlsx", header=[2])
+df.head()
+
+# %%
+df.drop(df.columns[6:], axis=1, inplace=True)
+df.drop([114,115], inplace=True)
+df[110:120]
+
+# %%
+df.tail()
+
+# %%
+df["Married"].replace(to_replace="Kid", value="NM", inplace=True)
+df["Day"].replace(to_replace="Tues", value="Tue", inplace=True)
+
+def cap_first_letter(word):
+    return word[0].upper() + word[1:].lower()
+
+df["Day"] = df["Day"].apply(cap_first_letter)
+
+df.loc[df["Day"] == "mon"]  # Should be empty
+
+# %%
+df["Time"] = df["Time"].apply(lambda x: x + 12 if 1 <= x <= 8 else x)
+df[20:30]
+
+# %%
+df["Time"] = df["Time"].astype(int)
+df["Age"] = df["Age"].astype(int)
+df.head()
+
+
